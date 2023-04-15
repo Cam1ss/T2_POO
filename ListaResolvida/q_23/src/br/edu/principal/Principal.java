657ef6ef6f8c422054469cacee1536e664f59d49
Camila Braúna <125037138+Cam1ss@users.noreply.github.com>
2023-04-15 11:57:29 -0300

    Questão

--- ListaResolvida/q_23/src/br/edu/principal/Principal.java
+++ ListaResolvida/q_23/src/br/edu/principal/Principal.java
@@ -12,35 +12,35 @@ public class Principal {
 		
 		double sal_min, sal_bruto, sal_liquido, nht, coeficiente = 0, auxilio, grat, pecent, imposto = 0;
 		
-		System.out.println("Digite a letra (Maiuscula) correpodente ao seu Turno: "
+		System.out.println("Digite a letra correpodente ao seu Turno: "
 				+ "\n(M) - Matutinno | (V) - Vespertino | (N) - Noturno ");
-		turno = sc.nextLine().charAt(0);
+		turno = sc.nextLine().toUpperCase().charAt(0);
 		
-		System.out.println("Digite a letra (Maiuscula) correspondente a sua Categoria: "
-				+ "\n(O) - Operário | (G) - Gerente ");
-		categoria = sc.nextLine().charAt(categoria);
+		System.out.println("Digite a letra correspondente a sua Categoria: "
+				+ "\n(O) - OperÃ¡rio | (G) - Gerente ");
+		categoria = sc.nextLine().toUpperCase().charAt(categoria);
 		
-		System.out.println("Digite o valor do seu Salário: ");
+		System.out.println("Digite o valor do seu SalÃ¡rio: ");
 		sal_min = sc.nextDouble();
 		
-		System.out.println("Digite o número de horas trabalhadas:");
+		System.out.println("Digite o nÃºmero de horas trabalhadas:");
 		nht = sc.nextInt();
 	
 		if (turno == 'M'){
 			coeficiente = ((double)((10/100.0) * sal_min));
-			System.out.println("Esse é seu coeficiente: " + coeficiente);
+			System.out.println("Esse Ã© seu coeficiente: " + coeficiente);
 		  } 
 		  else if (turno == 'V') {
 			  coeficiente = (double)((15/100.0) * sal_min);
-			  System.out.println("Esse é seu coeficiente: " + coeficiente);
+			  System.out.println("Esse Ã© seu coeficiente: " + coeficiente);
 		  } 
 		  else if (turno == 'N') {
 			  coeficiente = (double)((12/100.0) * sal_min);
-			  System.out.println("Esse é seu coeficiente: " + coeficiente);
+			  System.out.println("Esse Ã© seu coeficiente: " + coeficiente);
 		  }
 		 
 		  sal_bruto = (double)(nht * coeficiente);
-		  System.out.println("Esse é seu Salário Bruto: " + sal_bruto);
+		  System.out.println("Esse Ã© seu SalÃ¡rio Bruto: " + sal_bruto);
 		  
 		  if (categoria == 'O') {
 		  	if (sal_bruto >= 300) {
@@ -56,22 +56,22 @@ public class Principal {
 		  		imposto = (double)((4/100.0) * sal_bruto);
 		  	}
 		}
-			System.out.println("Este é seu imposto: " + imposto);
+			System.out.println("Este Ã© seu imposto: " + imposto);
 		  
 			if (turno == 'N' & nht > 80) {
 			  grat = 50;
 		  } else {
 			  grat = 30;
-		  } System.out.println("Esse é a sua gratificação: " + grat);
+		  } System.out.println("Esse Ã© a sua gratificaÃ§Ã£o: " + grat);
 		  
 		  if (categoria == 'O' && coeficiente <= 25) {
 			  auxilio = (double)((1/3) * sal_bruto);
 		  } else {
 			  auxilio = (double)((1/2) * sal_bruto);
-		  } System.out.println("Este é seu auxílio: " + auxilio);
+		  } System.out.println("Este Ã© seu auxÃ­lio: " + auxilio);
 		  
 		  sal_liquido = sal_bruto - imposto + grat + auxilio;
-		  System.out.println("Esre é o seu Salário Liquido: " + sal_liquido);
+		  System.out.println("Esre Ã© o seu SalÃ¡rio Liquido: " + sal_liquido);
 		  
 		  if (sal_liquido < 350) {
 			  System.out.println("Mal Remunerado");
